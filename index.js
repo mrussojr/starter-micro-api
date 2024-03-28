3a0cc64a90ae52e2cbc866fe7e401c616937649f
mrussojr <mrussojr@users.noreply.github.com>
2024-03-28 13:31:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const bots = require('./lib/bots.json');
 var http = require('http');
+const bodyParser = require('body-parser');
 
 http.createServer(function (req, res) {
     let found = false;
     let imgUrl = 'https://i.imgflip.com/';
     let imgIndex = 0;
 
+    req.body = bodyParser.json(req);
+    
     console.log(req.body);
 
     res.write(imgUrl);
