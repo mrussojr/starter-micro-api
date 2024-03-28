91b117f66b8760fb01d301cac21bb9daec4ed548
mrussojr <mrussojr@users.noreply.github.com>
2024-03-28 13:15:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+const bots = require('./lib/bots.json');
 var http = require('http');
+
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
+    let found = false;
+    let imgUrl = 'https://i.imgflip.com/';
+    let imgIndex = 0;
+
+    //console.log(req.body.body.content);
+
+    bots.bots.map((b) => {
+        if(!found){
+            b.keyWords.map((k) => {
+                if(req.body.body.content.match(k)){
+                    found = true;
+                }
+            })
+
+            if(found){
+                if(b.randomImg === "true"){
+                    imgIndex = Math.floor(Math.random() * b.imgUrls.length);
+                }
+
+                if(b.overrideUrl === "true"){
+                    imgUrl = b.imgUrls[imgIndex];
+                }else{
+                    imgUrl += b.imgUrls[imgIndex];
+                }
+            }
+        }
+    });
+
+    if(found){
+        res.write(imgUrl);
+    }else{
+        res.write();
+    }
 }).listen(process.env.PORT || 3000);
