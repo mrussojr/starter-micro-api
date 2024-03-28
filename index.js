a868a658040a29825506c9daf5aa6fe226ca62e9
mrussojr <mrussojr@users.noreply.github.com>
2024-03-28 13:30:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const bots = require('./lib/bots.json');
 var http = require('http');
 
 http.createServer(function (req, res) {
-    req.on('data', (chunk) => {
-    // Do something with the data
-        let found = false;
-        let imgUrl = 'https://i.imgflip.com/';
-        let imgIndex = 0;
-    
-        console.log(chunk);
-    
-        res.write(imgUrl);
-    
-        res.end();
-    });
+    let found = false;
+    let imgUrl = 'https://i.imgflip.com/';
+    let imgIndex = 0;
+
+    console.log(req.body);
+
+    res.write(imgUrl);
+
+    res.end();
 }).listen(process.env.PORT || 3000);
